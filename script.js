e2e3902a62e9a86e0658c1c4f472ba28f8bdfd22
sud018 <sudheeraya18@gmail.com>
2021-06-05 00:52:44 +0530

    second commit

--- script.js
+++ script.js
@@ -2,19 +2,16 @@ function doctorinfo() {
   document.getElementById("doctor-info").style.display = "inline";
   document.getElementById("student-info").style.display = "none";
   document.getElementById("other-info").style.display = "none";
-
 }
 
 function studentinfo() {
   document.getElementById("student-info").style.display = "inline";
   document.getElementById("doctor-info").style.display = "none";
   document.getElementById("other-info").style.display = "none";
-
 }
 
 function otherinfo() {
   document.getElementById("other-info").style.display = "inline";
   document.getElementById("student-info").style.display = "none";
   document.getElementById("doctor-info").style.display = "none";
-
 }
